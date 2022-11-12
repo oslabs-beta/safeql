f39d66e267fd0ace8beb7ece5532217c000df806
Christopher Kim <christophminkim@gmail.com>
2022-11-12 13:51:04 -0800

    setup prettier

--- components/test.js
+++ components/test.js
@@ -1,7 +1,5 @@
-function Header (){
-  return (
-    <div>This is a Header Component!</div>
-  )
+function Header() {
+  return <div>This is a Header Component!</div>;
 }
 
-export default Header
+export default Header;
