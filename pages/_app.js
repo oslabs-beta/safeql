278bea475baa050547aeb30fb2ea1187093b6b00
Christopher Kim <christophminkim@gmail.com>
2022-11-29 18:10:43 -0800

    link tailwindcss to app, ignore generated tailwind file

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,15 @@
 import '../styles/globals.css';
 import '../styles/components.css';
+import '../styles/output.css';
 import Header from '../components/Header';
-import { ContextProvider } from "../src/context"; 
-
+import { ContextProvider } from '../src/context';
 
 function MyApp({ Component, pageProps }) {
-  
   return (
-      <ContextProvider>
-        <Header />
-        <Component {...pageProps} />
-      </ContextProvider>
+    <ContextProvider>
+      <Header />
+      <Component {...pageProps} />
+    </ContextProvider>
   );
 }
 
