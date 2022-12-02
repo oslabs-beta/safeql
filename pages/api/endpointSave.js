490c2d54e78a1e5c55cb530d698ba6234421cf9f
CTwibell0 <collin.twibell@gmail.com>
2022-12-01 20:01:10 -0600

    enpointsave and usersCreate working

--- pages/api/endpointSave.js
+++ pages/api/endpointSave.js
@@ -1,19 +1,22 @@
-import conn from "../../lib/db"
+import db from "../../lib/db"
+
+//save endpoint functionality - Save a single endpoint with associated user id.
 
 export default async function (req, res){
     try{
         console.log('req.body: ', req.body);
         const query = `INSERT INTO endpointhistory(endpoint, userid)
-        VALUES ($1, $2)`
+        VALUES ($1, $2) RETURNING *`
         const values = [req.body.endpoint, req.body.userid]
         const result = await db.query(
             query,
             values
         );
         console.log("result: ", result);
-        return res.status(200);
+        return res.status(200).json(result.rows[0]);
     }
     catch(err){
         console.log(err);
+        res.status(400).send(error);
     }
 }
--- pages/api/usersCreate.js
+++ pages/api/usersCreate.js
@@ -1,19 +1,22 @@
 const db = require('../../lib/db')
 
+//create user functionality - creates a user and password
+
 export default async function (req, res){
     try{
         console.log('req.body: ', req.body);
         const query = `INSERT INTO users(username, password)
-        VALUES ($1, $2)`
+        VALUES ($1, $2) RETURNING *`
         const values = [req.body.username, req.body.password]
         const result = await db.query(
             query,
             values
         );
         console.log("result: ", result);
-        return res.status(200);
+        return res.status(200).json(result.rows[0]);
     }
     catch(err){
         console.log(err);
+        res.status(400).send(err);
     }
 }
