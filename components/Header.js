1cf661f1dc093312c9de054cc13c9769e03876eb
Christopher Kim <christophminkim@gmail.com>
2022-12-02 17:02:12 -0800

    add padding and adjust size on header

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 function Header() {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center p-2'>
       <Image
         src='/safeql-light.png'
         alt='SafeQL Logo'
-        width={200}
-        height={16}
+        width={180}
+        height={14}
       />
     </div>
   );
