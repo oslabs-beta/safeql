911ed733265374158ae4b3003d17c6bdc0bd0485
Christopher Kim <christophminkim@gmail.com>
2022-12-21 18:05:23 -0800

    Format with prettier

--- src/cm6Theme.js
+++ src/cm6Theme.js
@@ -1,23 +1,20 @@
 import { EditorView } from '@codemirror/view';
 
-
 const fixedHeightEditor = EditorView.theme({
-   "&": {
-    height: "600px",
+  '&': {
+    height: '600px',
     backgroundColor: '#5277AF',
-    color: '#344E77'
-    },
-    ".cm-scroller": {overflow: "auto"},
-    ".cm-gutters": {
-        backgroundColor: '#5277AF',
-        color: '#344E77',
-        border: "none"
-    },
-    ".cm-activeLineGutter": {
-        backgroundColor: '#7495C7'
-    },
-})
-
-
+    color: '#344E77',
+  },
+  '.cm-scroller': { overflow: 'auto' },
+  '.cm-gutters': {
+    backgroundColor: '#5277AF',
+    color: '#344E77',
+    border: 'none',
+  },
+  '.cm-activeLineGutter': {
+    backgroundColor: '#7495C7',
+  },
+});
 
 export { fixedHeightEditor };
