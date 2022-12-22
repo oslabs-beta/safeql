aa87444af743c2b3bb7180423bf42ae74997ea8a
Christopher Kim <christophminkim@gmail.com>
2022-12-21 18:09:26 -0800

    Format with Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import '../styles/components.css';
 import '../styles/output.css';
 import Header from '../components/Header';
 import { ContextProvider } from '../src/context';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div id='main-container' className='bg-blue-400 text-white box-border'>
+    <div
+      id='main-container'
+      className='bg-blue-400 text-white box-border'
+    >
       <ContextProvider>
         <Header />
         <Component {...pageProps} />
--- pages/api/endpointDelete.js
+++ pages/api/endpointDelete.js
@@ -1,20 +1,16 @@
-const db = require('../../lib/db')
+const db = require('../../lib/db');
 
-export default async function (req, res){
-    try{
-        console.log('req.body: ', req.body);
-        const query = `DELETE FROM endpointhistory WHERE endpoint = '${req.body.endpoint}' AND userid = '${req.body.userid}' RETURNING *`;
+export default async function (req, res) {
+  try {
+    console.log('req.body: ', req.body);
+    const query = `DELETE FROM endpointhistory WHERE endpoint = '${req.body.endpoint}' AND userid = '${req.body.userid}' RETURNING *`;
 
-        // const values = [req.body.endpoint, req.body.userid]
-        const result = await db.query(
-            query
-        );
-        console.log("result: ", result);
-        return res.status(200).json(result.rows[0]);
-    }
-    catch(err){
-        console.log(err);
-        res.status(400).send(err);
-    }
+    // const values = [req.body.endpoint, req.body.userid]
+    const result = await db.query(query);
+    console.log('result: ', result);
+    return res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
 }
-
