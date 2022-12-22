c43e6f91f3b41588baea2fc9aac0ac41481693ae
Christopher Kim <christophminkim@gmail.com>
2022-12-21 18:05:03 -0800

    Add contextType alias

--- src/context.tsx
+++ src/context.tsx
@@ -1,51 +1,62 @@
-import { createContext, useState, Dispatch, SetStateAction, ReactNode } from "react";
+import {
+  createContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+  ReactNode,
+} from 'react';
 
 //create type for Context
 type ContextType = {
-  url:string,
-  setUrl: 
-}
+  url: string;
+  setUrl: Dispatch<SetStateAction<string>>;
+  response: string;
+  setResponse: Dispatch<SetStateAction<string>>;
+  introspectionOn: boolean;
+  setIntrospectionOn: Dispatch<SetStateAction<boolean>>;
+  analysisData: AnalysisType;
+  setAnalysisData: Dispatch<SetStateAction<AnalysisType>>;
+  isSecurity: boolean;
+  setIsSecurity: Dispatch<SetStateAction<boolean>>;
+};
 
-type AnalysisType ={
-  querySpeed: number | null
-}
-
+type AnalysisType = {
+  querySpeed: number | null;
+};
 
 //create context
 const Context = createContext<ContextType>({});
-Context.displayName = "Query Context"
+Context.displayName = 'Query Context';
 
 //add context provider
-const ContextProvider = ({children}) => {
+const ContextProvider = ({ children }) => {
   //create state variables
   const [url, setUrl] = useState<string>('');
   const [response, setResponse] = useState<string>('');
-  const [introspectionOn, setIntrospectionOn] = useState<boolean>(true)
+  const [introspectionOn, setIntrospectionOn] = useState<boolean>(true);
   const [analysisData, setAnalysisData] = useState<AnalysisType>({
-    querySpeed: null,  
+    querySpeed: null,
   });
-  const [ isSecurity, setIsSecurity] = useState<boolean>(false)
+  const [isSecurity, setIsSecurity] = useState<boolean>(false);
 
   //Initialize the state
-const initialState = {
-  url,
-  setUrl,
-response,
-setResponse,
-  introspectionOn,
-  setIntrospectionOn,
-analysisData,
-setAnalysisData,
-  isSecurity,
-  setIsSecurity
-}
+  const initialState = {
+    url,
+    setUrl,
+    response,
+    setResponse,
+    introspectionOn,
+    setIntrospectionOn,
+    analysisData,
+    setAnalysisData,
+    isSecurity,
+    setIsSecurity,
+  };
 
   return (
-        // this is the provider providing state
-        <Context.Provider value={initialState}>
-            {children}
-        </Context.Provider>
-    );
+    // this is the provider providing state
+    <Context.Provider value={initialState}>{children}</Context.Provider>
+  );
 };
 
-export {Context, ContextProvider};
+export { Context, ContextProvider };
