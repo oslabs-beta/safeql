e17883153988f995dd4f64d3b09d0e62e0003121
Mariko Iwata <3701668+MarikoIwata@users.noreply.github.com>
2023-01-14 13:35:22 -0500

    added default schema in editor, but not run

--- components/schemaParser/SchemaInput.js
+++ components/schemaParser/SchemaInput.js
@@ -11,7 +11,13 @@ import { parseSchema } from "../../src/parseSchema";
 
 export const SchemaInput = (props) => {
   const editor = useRef();
-  const [schema, setSchema] = useState('');
+  const [schema, setSchema] = useState(
+  `type Cohort {
+    id: ID
+    studentCount: Number
+    region: String
+  }`
+  );
 
   const updateSchema = EditorView.updateListener.of((v) => {
     setSchema(v.state.doc.toString());
@@ -69,6 +75,7 @@ export const SchemaInput = (props) => {
     <section className='bg-blue-300 mt-2 rounded-t-lg-1 w-1/3 box-border min-w-fit mr-1'>
       <div className='flex justify-between px-1 font-bold h-15 px-2 py-2 content-center'>
         <h2 className='flex text-xl p-2'>Schema</h2>
+        
         <button
           className='text-md bg-blue-200 rounded-lg-1 p-2 min-w-fit'
           onClick={submitSchema}
