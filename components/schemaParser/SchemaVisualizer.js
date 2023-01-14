48ea7bd76d61a64ceae3c85f51818b9b53508f68
Mariko Iwata <3701668+MarikoIwata@users.noreply.github.com>
2023-01-14 14:19:34 -0500

    able to move around nodes

--- components/schemaParser/SchemaVisualizer.js
+++ components/schemaParser/SchemaVisualizer.js
@@ -1,33 +1,64 @@
 //import our schema parser function
 import SchemaDisplay from "./SchemaDisplay";
-import ReactFlow, {Background, Controls} from 'reactflow';
+import {useState, useCallback} from 'react';
+import ReactFlow, {Background, Controls, applyEdgeChanges, applyNodeChanges} from 'reactflow';
 import 'reactflow/dist/style.css'
 
-function SchemaVisualizer(props) {
-  //LOGIC TO TRANSFORM SCHEMA INTO VISUALIZATION COMPONENTS
-  //1. Pass our props.schema from our input into our parser function
-  //SchemaParser(props.schema) => out comes our array of objects 
-  //2. then display all this into visualization components
-  // const postParsing = parseSchema(props.schema);
-
-//displaySchema component: (array logic done in Visualizer)
-    //take each object and display each subfield with Type
-    //table
-//stretch - massive visualization with a vis library
-   console.log('props.parsedSchema', props.parsedSchema)
-  let schemas = [];
-
-  for (let i = 0; i < props.parsedSchema.length; i++) {
-    const {name, fields} = props.parsedSchema[i]
-    schemas.push(<SchemaDisplay name={name} fields={fields} key={i}/>)
-    console.log('in for loop, schemaVisualizer');
+const initialNodes = [
+  {
+    id: '1', //required for react flow
+    position: {x:10, y:100}, //required for react flow
+    data: {label:'Hello'},
+    type: 'input',
+  },
+  {
+    id:'2',
+    position: {x:200, y:200},
+    data: {label: 'World'}
   }
-  console.log('schemas array', schemas)
+];
+
+const initialEdges = [
+  {
+    id: '1-2',
+    source: '1', //where edge begins
+    target: '2', //where edge ends
+    type: 'step'//default is curved line
+  }
+]
+
+
+function SchemaVisualizer(props) {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes)=> setNodes((nds)=>applyNodeChanges(changes, nds)), []
+  );
+
+  const onEdgesChange = useCallback(
+    (changes)=> setEdges((eds)=> applyEdgeChanges(changes, eds)), []
+  );
   
+  // console.log('props.parsedSchema', props.parsedSchema)
+  // let schemas = [];
+
+  // for (let i = 0; i < props.parsedSchema.length; i++) {
+  //   const {name, fields} = props.parsedSchema[i]
+  //   schemas.push(<SchemaDisplay name={name} fields={fields} key={i}/>)
+  //   console.log('in for loop, schemaVisualizer');
+  // }
+  
+  // console.log('schemas array', schemas)  
   return (
     <div className="w-full">
-    {schemas}
-        <ReactFlow>
+    {/* schemas */}
+        <ReactFlow 
+          nodes={nodes}
+          onNodesChange={onNodesChange}
+          edges ={edges}
+          onEdgesChagne={onEdgesChange}
+        >
           <Background />
           <Controls />
         </ReactFlow>
