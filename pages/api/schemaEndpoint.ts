0bfa1c96a036fc3ca53b8204631d8b051914351b
Etana Kopin <claws.33@gmail.com>
2023-01-19 20:31:22 -0700

    circularCheck works great imported to the api

--- pages/api/schemaEndpoint.ts
+++ pages/api/schemaEndpoint.ts
@@ -1,10 +1,13 @@
 import { parseSchema } from "../../src/parseSchema";
 import { NextApiRequest, NextApiResponse } from 'next';
+import { circularCheck } from '../../src/schemaFuncs/circularCheck'
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const schema  = req.body;
   try {
     const postParsing = await parseSchema(schema) // expect parsedSchema [] //however undefined -> "test"
+    res.locals.circularSchema = await circularCheck(postParsing);
+    console.log(circularCheck) //works great! What do we do with this data now?
     return res.status(200).json(postParsing)
   } catch (err) {
     console.log(err);
