4042be200d4e0fe600d18ad672e50269106f5952
Christopher Kim <christophminkim@gmail.com>
2023-01-30 17:56:44 -0800

    Add styling for navigation

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,32 +1,32 @@
-import { useRouter } from "next/router";
-import Link from "next/link";
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const navigationRoutes = [
   {
     name: 'Home',
-    path: '/'
+    path: '/',
   },
   {
-    name : 'SchemaParser',
-    path: '/schemaParser'
-  }
+    name: 'SchemaParser',
+    path: '/schemaParser',
+  },
 ];
 
 function NavBar() {
   const router = useRouter();
   return (
-    <div className='flex justify-center p-2'>
-    <nav>
-      {navigationRoutes.map((link, index)=> {
-        return(
-          <ul>
-            <Link href={link.path}>
-              <li key={index}>{link.name}</li>
-            </Link>
-          </ul>
-        )
-      })}
-    </nav>
+    <div className='flex justify-center mr-7 mt-1 absolute right-0'>
+      <nav className='flex flex-row justify-center content-center'>
+        {navigationRoutes.map((link, index) => {
+          return (
+            <ul className='p-2 mx-2 bg-blue-300 rounded-md hover:bg-blue-200 cursor-pointer'>
+              <Link href={link.path}>
+                <li className='font-bold' key={index}>{link.name}</li>
+              </Link>
+            </ul>
+          );
+        })}
+      </nav>
     </div>
   );
 }
