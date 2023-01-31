9ce8983410db848d147ad94dd55f0d176bc5c6c9
Christopher Kim <christophminkim@gmail.com>
2023-01-30 17:58:24 -0800

    Remove NavBar from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     >
       <ContextFunctionality>
         <Header />
-        <Navbar />
         <Component {...pageProps} />
       </ContextFunctionality>
     </div>
