f54ce6e74d3ec56440b6ab6a9dc7c5766748b6fd
Mariko Iwata <3701668+MarikoIwata@users.noreply.github.com>
2023-02-01 00:49:51 -0600

    added customTableNode hard coded tables

--- components/schemaParser/CustomTableNode.js
+++ components/schemaParser/CustomTableNode.js
@@ -3,9 +3,21 @@ import React, { memo } from 'react';
 // CustomTableNode will return a unique node for each SchemaDisplayReactFlow Custom Nodes
 
 function customNodeFormat(props){
-  console.log('customNodeFormat props',props)
+console.log('customNodeFormat props', props)
 
-}
+  return(
+    <>
+      <table className='border-solid border-white bg-blue-300 mt-2 ml-1 rounded-t-lg-1 w-1/3 box-border min-w-fit'>
+        <tr key={id}>
+        
+        {/* loop through data props to make key */}
+        {/* loop through data props to make value of that key */}
+
+        </tr>
+      </table>
+    </>
+  );
+};
 
   function CustomTableNode(props){
     console.log('props in CustomTableNode',props)
@@ -16,17 +28,24 @@ function customNodeFormat(props){
         <div>{tableName}</div>
 
         <customNodeFormat
-          region = {props.data.region}
+          id = {props.id}
         />
         <table>
           <tr>
             <td>{props.data.id}</td>
-          </tr>
-          <tr>
             <td>{props.data.id}</td>
           </tr>
           <tr>
-            <td>{props.data.id}</td>
+            <td>{props.data.tableName}</td>
+            <td>{props.data.tableName}</td>
+          </tr>
+          <tr>
+            <td>{props.data.studentCount}</td>
+            <td>{props.data.studentCount}</td>
+          </tr>
+          <tr>
+            <td>{props.data.region}</td>
+            <td>{props.data.region}</td>
           </tr>
         </table>
        
