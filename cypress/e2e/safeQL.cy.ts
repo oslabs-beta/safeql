ad980065ef6f47d34a21962904eaf2956e2b1992
CTwibell0 <collin.twibell@gmail.com>
2023-02-06 21:36:13 -0600

    cypresss E2E testing
    Co-authored-by: Victor He <victorhe33@gmail.com>
    Co-authored-by: Christopher Kim <christophminkim@gmail.com>

--- cypress/e2e/safeQL.cy.ts
+++ cypress/e2e/safeQL.cy.ts
@@ -1,27 +1,41 @@
-describe('visit query homepage', () => {
-  it('via direct URI passes', () => {
-    cy.visit('http://localhost:3000/')
+xdescribe('visit query homepage', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Checking navigation', () => {
     cy.contains('Schema')
-    cy.contains('SchemaParser').click() // Click on first el containing 'Welcome'
+    cy.get(':nth-child(2) > a > .font-bold').click() // Click on first el containing 'Welcome'
     cy.contains('Sandbox').should('not.exist')
-
-
+    //check url
+    cy.location('pathname').should('eq', '/schemaParser')
+    //click on homepage
+    cy.get(':nth-child(1) > a > .font-bold').click()
+    //check for homepage
+    cy.location('pathname').should('eq', '/')
   })
-  it('click schemaParser page passes', () => {
-    // cy.visit('http://localhost:3000/')
-    // cy.get('li').click() // Click on button
-    // cy.focused().click() // Click on el with focus
 
+  it('check query sending/response', () => {
+    cy.get('.bg-blue-100').type('https://rickandmortyapi.com/graphql')
+    cy.get('.cm-activeLine').type(`query { characters(page: 1) { info { count } } }`, { parseSpecialCharSequences: false })
+    cy.get('.text-md').click()
+    cy.get('.response > .cm-editor > .cm-scroller > .cm-content').should('have.text', '{  "data": {    "characters": {      "info": {        "count": 826      }    }  }}')
+    cy.contains('Metric Analysis')
+    cy.get('.ml-1 > .justify-evenly > :nth-child(2)').click()
+    cy.contains('Security Analysis')
+    cy.contains('Introspection: On')
   })
 })
 
-// describe('visit schemaparser page', () => {
-//   it('via direct URI passes', () => {
-//     cy.visit('http://localhost:3000/schemaParser')
-//     cy.contains('Schema')
-//     cy.contains('Sandbox').should('not.exist')
-//   })
-//   it('via click from homepage passes', () => {
-//     cy.visit('http://localhost:3000/')
-//   })
-// })
+describe('visit schemaparser page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/schemaParser')
+  });
+  
+  it('Inputting schema', () => {
+    cy.get('.cm-activeLine').type('type Cohort { id: ID studentCount: Number region: String }', { parseSpecialCharSequences: false })
+    cy.get('.text-md').click()
+    cy.get('.border-solid').should('exist')
+
+  })
+})
